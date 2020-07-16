ceccaa601adb3b2b8cb7de3d8a3aa3b6a412cde3
AndrasHorinka <horinka.andras@gmail.com>
2020-07-16 11:36:34 +0200

    prototype 1.2 gas pedal

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    public PlayerController()
+    {
+        Speed = 5.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+    }
+}
+
