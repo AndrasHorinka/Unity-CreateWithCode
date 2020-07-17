df44556e8f83082fe834176ea894459a266d6796
AndrasHorinka <horinka.andras@gmail.com>
2020-07-17 10:59:47 +0200

    Prototype1 - Step into the Driver's Seat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,23 +2,27 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed;
-
-    public PlayerController()
-    {
-        Speed = 5.0f;
-    }
+    private float Speed;
+    private float TurnSpeed;
+    private float HorizontalInput;
+    private float VerticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Speed = 5.0f;
+        TurnSpeed = 25.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * Speed * VerticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * TurnSpeed * HorizontalInput);
 
         if (Speed < 80)
         {
