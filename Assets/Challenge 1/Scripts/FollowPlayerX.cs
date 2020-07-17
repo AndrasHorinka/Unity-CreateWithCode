c011fea79f404fa897595dff9f5ceb0817475583
AndrasHorinka <horinka.andras@gmail.com>
2020-07-17 11:39:05 +0200

    challange 1 - aeroplane

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject Plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(15, 0, 0);
+        transform.rotation = Quaternion.Euler(0, 270, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PropellerRotationX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotationX : MonoBehaviour
+{
+
+    private float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // reset propeler if turned around
+        if (transform.rotation.z == 180.0f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        transform.Rotate(new Vector3(0, 0, 30) * rotationSpeed * Time.deltaTime);
+    }
+}
